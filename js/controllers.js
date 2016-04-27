870214bd8140dfcf971fc8a680284bb545f76b12
cristian012 <camilos94-25@hotmail.com>
2016-04-27 18:00:10 -0500

    Esqueleto base para el desarrollo Backend

--- js/controllers.js
+++ js/controllers.js
@@ -7,10 +7,21 @@
 	        center: {
 	            lat: -1.259311,
 	            lng: -78.524037,
-	            zoom: 6
+	            zoom: 8
 	        },
 	        defaults: {
 	            scrollWheelZoom: false
 	        }
 	    });
+
+		$scope.templates = [
+			{nombre: 'introduccion', url: 'templates/introduction.html'},
+			{nombre: 'indicadores', url: 'templates/indicators.html'}
+		];
+
+		$scope.template = $scope.templates[0];
+
+		$scope.comenzar = function(index){
+			$scope.template = $scope.templates[index];
+		}
 	});
