4188038f14324d42d52c178d2693f29ef543ee66
Juan Leon <juansele@gmail.com>
2016-05-02 11:19:45 -0400

    adds several functionality

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,35 @@
 // File controllers aplication
 
 // 1. Main controller ----------------------------------------------------------------------------
-app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
+app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia, $filter){
+    // cosas estilosas
+    var estilo = {
+        color_zona: {
+            2: '#AA904A',
+            1: '#8A324E',
+            3: '#77C24F',
+            4: '#4061AC'
+        },
+        color_sector: {
+            "Educación": '#F37D30',
+            "Salud": '#64ACA3',
+            "Pobreza": '#AD9150',
+            "Empleo": '#31656E',
+            "Actividad Económica": '#76AB54'
+        },
+        formato_metrica: function(cantidad, tipo){
+            var resultado = "";
+            if (tipo == 'porcentaje') {
+                resultado = $filter('number')(100*cantidad, 1) + ' %';
+            } else if (tipo == 'entero'){
+                resultado = $filter('number')(cantidad, 0);
+            } else {
+                resultado = $filter('number')(cantidad, 1);
+            }
+            return resultado
+        }
+    };
+    
     // objeto con todo
     var chavimochic = {
         distritos: {},  // contiene la lista de distritos
@@ -9,22 +37,44 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
         ambiente: {},   // continee los datos de 'accion por el clima'
         sectores: {},   // contiene las metricas de cada sector
         metricas: {},   // contiene las propiedades de cada metrica
+        informacion: {},  // contiene los textos
         
         selector: {},   // contiene los valores de los selectores
+        simular: {},    // modelos de las casillas de simular
         
         varianza: {},   // contine la varianza para cada metrica
         resultado: {},  // contiene los Pvalues y quintiles para cada metrica
+        calificacion: {}, // contiene el promedio de Pvalues por distrito
         quintiles: {},  // contiene los quintiles de cada sector
         
+        distrito: {
+            seleccionado: "Selecciona un distrito",
+            drop: false,
+            sin_seleccionar: false
+        },
+        metrica_sector: {
+            metrica: "",
+            sector: ""
+        },
         mapa: {
             path: {},
-            center: {
-                lat: -1.259311,
-                lng: -78.524037,
-                zoom: 8
+            bounds: {
+                "northEast":{"lat":-7.348846969785414,"lng":-77.74749755859375},
+                "southWest":{"lat":-8.977323208440273,"lng":-79.99969482421875}
             },
             defaults: {
-                scrollWheelZoom: false
+                keyboard: false,
+                dragging: false,
+                doubleClickZoom: false,
+                scrollWheelZoom: false,
+                tap: false,
+                touchZoom: false,
+                zoomControl: false,
+                controls: {
+                    layers: {
+                        visible: false
+                    }
+                }
             },
             layers: {
                 overlays: {
@@ -39,6 +89,14 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
                 path: {
                     enable: ['click']
                 }
+            },
+            tiles: {
+                url: "http://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png",
+                //url: "http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png",
+                options: {
+                    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    //attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+                }
             }
         },
         
@@ -48,20 +106,25 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
             this.ambiente = raw.ambiente;
             this.sectores = raw.sectores;
             this.metricas = raw.metricas;
+            this.informacion = raw.informacion;
             
             for (metrica in this.raw_data){
                 this.selector[metrica] = {};
+                this.simular[metrica] = {};
                 for (distrito in this.raw_data[metrica]){
                     this.selector[metrica][distrito] = 'actual';
+                    this.simular[metrica][distrito] = false;
                 }
             }
             
             for (distrito in this.distritos){
                 this.mapa.path[distrito] = {
                     latlngs: this.distritos[distrito].coordinates,
-                    stroke: false,
-                    fillColor: '#ff69b4',
-                    fillOpacity: 0.4,
+                    stroke: true,
+                    color: '#eeeeee',
+                    weight: 1,
+                    fillColor: estilo.color_zona[this.distritos[distrito].zona],
+                    fillOpacity: 0.9,
                     type: 'polygon',
                     layer: 'distritos'
                 };
@@ -106,6 +169,15 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
                 }
             }
             
+            // Calificacion
+            for (distrito in this.distritos){
+                var promedio = 0;
+                for (metrica in this.metricas){
+                    promedio += this.resultado[metrica][distrito].p_value;
+                }
+                this.calificacion[distrito] = promedio/Object.keys(this.metricas).length;
+            }
+            
             // Quintiles Sectores
             for (distrito in this.distritos){
                 this.quintiles[distrito] = {};
@@ -118,10 +190,72 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
                     this.quintiles[distrito][sector] = Math.ceil(media_geom / 0.2);
                 }
             }
+        },
+        
+        seleccionar_distrito: function(distrito){
+            if (this.distrito.sin_seleccionar){
+                this.mapa.path[this.distrito.seleccionado].weight = 1;
+            }
+            this.distrito.seleccionado = distrito;
+            this.mapa.path[distrito].weight = 3;
+            this.distrito.sin_seleccionar = true;
+            this.distrito.drop = false;
+        },
+        
+        simular_metrica: function(metrica, distrito){
+            if(this.selector[metrica][distrito] == 'optimo'){
+                this.selector[metrica][distrito] = 'actual';
+            } else {
+                this.selector[metrica][distrito] = 'optimo';
+            }
+            this.calcular();
+        },
+        
+        simular_sector: function(sector, distrito){
+            for(metrica in this.sectores[sector]){
+                this.simular[metrica][distrito] = !this.simular[metrica][distrito];                
+                this.simular_metrica(metrica, distrito);
+            }
+        },
+        
+        ver_zonas: function(){
+            this.metrica_sector.sector = "";
+            this.metrica_sector.metrica = "";
+            for (distrito in this.distritos){
+                this.mapa.path[distrito] = {
+                    latlngs: this.distritos[distrito].coordinates,
+                    stroke: true,
+                    color: '#eeeeee',
+                    weight: 1,
+                    fillColor: estilo.color_zona[this.distritos[distrito].zona],
+                    fillOpacity: 0.9,
+                    type: 'polygon',
+                    layer: 'distritos'
+                };
+            }
+        },
+        
+        ver_metrica_mapa: function(metrica, sector){
+            this.metrica_sector.sector = "";
+            this.metrica_sector.metrica = metrica;
+            for (distrito in this.distritos){
+                this.mapa.path[distrito].fillColor = estilo.color_sector[sector];
+                this.mapa.path[distrito].fillOpacity = 1 - this.resultado[metrica][distrito].p_value;
+            }
+        },
+        
+        ver_sector_mapa: function(sector){
+            this.metrica_sector.metrica = "";
+            this.metrica_sector.sector = sector;
+            for (distrito in this.distritos){
+                this.mapa.path[distrito].fillColor = estilo.color_sector[sector];
+                this.mapa.path[distrito].fillOpacity = this.quintiles[distrito][sector]/5;
+            }
         }
     };
     
     $scope.chavimochic = chavimochic;
+    $scope.estilo = estilo;
     
     // bajamos los datos
     $http.get('data.json')
@@ -132,17 +266,6 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
 
     
     /////////
-    angular.extend($scope, {
-        center: {
-            lat: -10.280419,
-            lng: -76.408341,
-            zoom: 8
-        },
-        defaults: {
-            scrollWheelZoom: false
-        }
-    });
-
     $scope.templates = [
         {nombre: 'introduccion', url: 'templates/introduction.html'},
         {nombre: 'indicadores', url: 'templates/indicators.html'}
@@ -190,7 +313,7 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
 
     // Lanzador del Modal
 
-    $scope.showAdvanced = function(ev) {
+    $scope.mostrar_informacion = function(ev, dimension, distrito, informacion, color) {
         var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
 
         $mdDialog.show({
@@ -198,6 +321,12 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
             templateUrl: 'dialogInformation.tmpl.html',
             parent: angular.element(document.body),
             targetEvent: ev,
+            locals: {
+                dimension: dimension,
+                distrito: distrito,
+                informacion: informacion,
+                color: color
+            },
             clickOutsideToClose:true,
             fullscreen: useFullScreen
         })
@@ -211,11 +340,17 @@ app.controller('mainController', function($scope, $http, $mdDialog, $mdMedia){
     
     // eventos del mapa
     $scope.$on('leafletDirectivePath.map.click', function(event, args){
-        console.log(event, args);
+        chavimochic.seleccionar_distrito(args.modelName);
     });
 });
 
-function DialogController($scope, $mdDialog) {
+
+function DialogController($scope, $mdDialog, dimension, distrito, informacion, color) {
+  $scope.dimension = dimension;
+  $scope.distrito = distrito;
+  $scope.informacion = informacion;
+  $scope.color = color;
+    
   $scope.hide = function() {
     $mdDialog.hide();
   };
@@ -228,3 +363,22 @@ function DialogController($scope, $mdDialog) {
     $mdDialog.hide(answer);
   };
 }
+
+
+
+
+app.filter('orderDistritos', function() {
+  return function(items, calificacion, zona) {
+    var filtered = [];
+    angular.forEach(items, function(item, key) {
+      if(item.zona == zona){
+        item.key = key;
+        filtered.push(item);  
+      }
+    });
+    filtered.sort(function (a, b) {
+      return (calificacion[a.key] > calificacion[b.key] ? -1 : 1);
+    });
+    return filtered;
+  };
+});
